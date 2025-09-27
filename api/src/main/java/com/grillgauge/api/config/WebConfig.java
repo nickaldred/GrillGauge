b14e6ded9e874a6bebb81820826dbdf07b3cb445
nicka <nickaldred@hotmail.co.uk>
2025-09-27 13:09:11 +0100

    feat: ApiKeys working

--- api/src/main/java/com/grillgauge/api/config/WebConfig.java
+++ api/src/main/java/com/grillgauge/api/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.grillgauge.api.configuration;
+package com.grillgauge.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
