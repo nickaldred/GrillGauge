f70cca0cb425a33b5f91efa970bcff0ca3f31f0e
nicka <nickaldred@hotmail.co.uk>
2025-09-27 18:37:32 +0100

    feat: implemented user controller

--- api/src/main/java/com/grillgauge/api/controllers/UserController.java
+++ api/src/main/java/com/grillgauge/api/controllers/UserController.java
@@ -1,10 +1,35 @@
 package com.grillgauge.api.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.grillgauge.api.domain.entitys.User;
+import com.grillgauge.api.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public User storeUser(@RequestBody final User user) {
+        return userService.storeUser(user);
+    }
+
+    @DeleteMapping
+    public User deleteUser(final User user) {
+        return userService.deleteUser(user);
+    }
 
 }
