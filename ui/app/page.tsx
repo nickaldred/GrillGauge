a04871a0779c1e31b7e741795b1455c3bb178f65
nicka <nickaldred@hotmail.co.uk>
2025-10-12 05:23:48 +0100

    feat: get user request working

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Header } from "./components/header";
 import { ProbeCard } from "./components/probeCard";
 import { Probe } from "./types/types";
 
 export default function Home() {
-  const testProbe: Probe = {
+  const [user, setUser] = useState(null);
+
+  const testProbe = {
     id: 1,
     name: "Probe 1",
     currentTemp: 55,
@@ -13,6 +16,32 @@ export default function Home() {
     color: "red",
     connected: true,
   };
+
+  async function getData(url: string) {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    getData("http://localhost:8080/api/v1/user?email=swathi@gmail.com")
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      });
+  }, []);
 
   const handleUpdateTargetTemp = async (probeId: number, temp: number) => {};
   const handleUpdateName = async (probeId: number, name: string) => {};
@@ -28,9 +57,8 @@ export default function Home() {
         onUpdateName={handleUpdateName}
         onClick={onClick}
       />
-      <p className="text-xl font-semibold text-gray-700 mt-4">
-        Welcome to the Meat Thermometer Dashboard!
-      </p>
+      <p>Welcome to the Meat Thermometer Dashboard!</p>
+      <p>User is: {user ? JSON.stringify(user) : "Loading user..."}</p>
     </main>
   );
 }
