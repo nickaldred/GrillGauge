b1edcbe3d2ed31f5a8db293127bd70eb4781adb5
nicka <nickaldred@hotmail.co.uk>
2025-10-20 06:32:15 +0100

    feat: removed password hash

--- api/src/test/java/com/grillgauge/api/controllers/ProbeControllerIntTest.java
+++ api/src/test/java/com/grillgauge/api/controllers/ProbeControllerIntTest.java
@@ -30,61 +30,65 @@ import jakarta.persistence.criteria.CriteriaBuilder.In;
 @Transactional
 class ProbeControllerIntTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Autowired
-    private ReadingRepository readingRepository;
+        @Autowired
+        private ReadingRepository readingRepository;
 
-    @Autowired
-    private ProbeRepository probeRepository;
+        @Autowired
+        private ProbeRepository probeRepository;
 
-    @Autowired
-    private HubRepository hubRepository;
+        @Autowired
+        private HubRepository hubRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    private List<Reading> readings;
-    private User testUser;
-    private Hub testHub;
-    private Probe testProbe;
+        private List<Reading> readings;
+        private User testUser;
+        private Hub testHub;
+        private Probe testProbe;
 
-    @BeforeEach
-    void setUp() {
-        testUser = new User("nick@hotmail.co.uk", "Nick", "Bloggs", "1234");
-        userRepository.save(testUser);
-        testHub = new Hub(testUser, "apiKey", "Test Hub");
-        hubRepository.save(testHub);
-        testProbe = new Probe(1, testHub, testUser, (float) 200, "probe 1");
-        probeRepository.save(testProbe);
+        @BeforeEach
+        void setUp() {
+                testUser = new User("nick@hotmail.co.uk", "Nick", "Bloggs");
+                userRepository.save(testUser);
+                testHub = new Hub(testUser, "apiKey", "Test Hub");
+                hubRepository.save(testHub);
+                testProbe = new Probe(1, testHub, testUser, (float) 200, "probe 1");
+                probeRepository.save(testProbe);
 
-        Reading reading1 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T10:00:00Z"));
-        Reading reading2 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T11:00:00Z"));
-        Reading reading3 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T12:00:00Z"));
-        Reading reading4 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T12:30:00Z"));
+                Reading reading1 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T10:00:00Z"));
+                Reading reading2 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T11:00:00Z"));
+                Reading reading3 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T12:00:00Z"));
+                Reading reading4 = new Reading(testProbe, (float) 180, Instant.parse("2024-01-01T12:30:00Z"));
 
-        readings = List.of(reading1, reading2, reading3, reading4);
-        readingRepository.saveAll(readings);
-    }
+                readings = List.of(reading1, reading2, reading3, reading4);
+                readingRepository.saveAll(readings);
+        }
 
-    @Test
-    void testGetReadingsForProbeBetween() throws Exception {
-        // Given
-        String start = "2024-01-01T10:30:00Z";
-        String end = "2024-01-01T12:00:00Z";
+        @Test
+        void testGetReadingsForProbeBetween() throws Exception {
+                // Given
+                String start = "2024-01-01T10:30:00Z";
+                String end = "2024-01-01T12:00:00Z";
 
-        // When / Then
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/v1/probe/{probeId}/readings/between", testProbe.getId())
-                .param("start", start)
-                .param("end", end))
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk())
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath("$.length()")
-                        .value(2))
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath("$[0].timeStamp")
-                        .value("2024-01-01T11:00:00Z"))
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath("$[1].timeStamp")
-                        .value("2024-01-01T12:00:00Z"));
-    }
+                // When / Then
+                mockMvc.perform(MockMvcRequestBuilders
+                                .get("/api/v1/probe/{probeId}/readings/between", testProbe.getId())
+                                .param("start", start)
+                                .param("end", end))
+                                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status()
+                                                .isOk())
+                                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers
+                                                .jsonPath("$.length()")
+                                                .value(2))
+                                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers
+                                                .jsonPath("$[0].timeStamp")
+                                                .value("2024-01-01T11:00:00Z"))
+                                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers
+                                                .jsonPath("$[1].timeStamp")
+                                                .value("2024-01-01T12:00:00Z"));
+        }
 }
