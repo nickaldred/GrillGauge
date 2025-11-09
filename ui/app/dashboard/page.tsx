7c07132d11cd56fe762c2515fac8988cd0922227
nicka <nickaldred@hotmail.co.uk>
2025-11-09 08:21:45 +0000

    feat: added a log in page

--- ui/app/dashboard/page.tsx
+++ ui/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import { Header } from "./components/dashboardHeader";
 import { DashboardPage } from "./components/dashboardPage";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/"); // Redirect to home if not logged in
+    }
+  }, [status, router]);
+
   return (
     <main className="p-6">
       <Header />
