8c1c3e49f68253f3897969a4f651137283d28ac2
nicka <nickaldred@hotmail.co.uk>
2025-11-09 07:16:11 +0000

    feat: converted client to client side

--- ui/app/dashboard/page.tsx
+++ ui/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/dashboardHeader";
-import { DashboardComponent } from "./components/dashboardComponent";
+import { DashboardPage } from "./components/dashboardPage";
 
 export default function Dashboard() {
   return (
     <main className="p-6">
       <Header />
-      <DashboardComponent />
+      <DashboardPage />
     </main>
   );
 }
