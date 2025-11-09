023c20a18a4981732ea641834f7fca7b8bfed8fb
nicka <nickaldred@hotmail.co.uk>
2025-11-09 06:37:16 +0000

    chore: moved to dashboard header

--- ui/app/dashboard/page.tsx
+++ ui/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "../components/header";
+import { Header } from "./components/dashboardHeader";
 import { DashboardComponent } from "./components/dashboardComponent";
 
 export default function Dashboard() {
