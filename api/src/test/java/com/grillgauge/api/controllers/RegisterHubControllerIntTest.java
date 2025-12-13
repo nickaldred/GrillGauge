fc6287da4bd879f844f9b7422ae2ba460ecf78b9
nicka <nickaldred@hotmail.co.uk>
2025-12-13 11:26:28 +0000

    feat: added test for confirm hub success

--- api/src/test/java/com/grillgauge/api/controllers/RegisterHubControllerIntTest.java
+++ api/src/test/java/com/grillgauge/api/controllers/RegisterHubControllerIntTest.java
@@ -7,12 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+
+import com.grillgauge.api.controllers.RegisterHubController.HubConfirmRequest;
 import com.grillgauge.api.controllers.RegisterHubController.HubRegistrationRequest;
+import com.grillgauge.api.domain.entitys.Hub;
+import com.grillgauge.api.domain.entitys.User;
+import com.grillgauge.api.domain.repositorys.HubRepository;
+import com.grillgauge.api.domain.repositorys.UserRepository;
 import com.grillgauge.api.services.RegisterHubService.HubRegistrationResponse;
 import com.grillgauge.api.utils.TestUtils;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegisterHubControllerIntTest {
+
+    private static final String REGISTER_URL = "/api/v1/register/register";
+    private static final String CONFIRM_URL = "/api/v1/register/confirm";
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -20,19 +29,55 @@ public class RegisterHubControllerIntTest {
     @Autowired
     private TestUtils testUtils;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private HubRepository hubRepository;
+
     @BeforeEach
     public void setup() {
         testUtils.clearDatabase();
     }
 
+    /**
+     * Helper method to register a hub.
+     * 
+     * @param model     The hub model.
+     * @param fwVersion The firmware version.
+     * @return The HubRegistrationResponse containing hub ID, OTP, and expiration
+     *         time.
+     */
+    private HubRegistrationResponse registerHub(final String model, final String fwVersion) {
+        HubRegistrationRequest registerRequest = new HubRegistrationRequest("ModelY", "2.0.0");
+        ResponseEntity<HubRegistrationResponse> registerResponse = restTemplate.postForEntity(REGISTER_URL,
+                registerRequest, HubRegistrationResponse.class);
+        assertTrue(registerResponse.getStatusCode().is2xxSuccessful());
+        HubRegistrationResponse registrationBody = registerResponse.getBody();
+        assertNotNull(registrationBody);
+        return registrationBody;
+    }
+
+    /**
+     * Create a test user.
+     * 
+     * @param email     The user's email.
+     * @param firstName The user's first name.
+     * @param lastName  The user's last name.
+     * @return The created User entity.
+     */
+    private User createUser(final String email, final String firstName, final String lastName) {
+        User testUser = new User(email, firstName, lastName);
+        return userRepository.save(testUser);
+    }
+
     @Test
     public void testRegisterHubSuccess() {
         // Given
-        String url = "/api/v1/register/register";
         HubRegistrationRequest request = new HubRegistrationRequest(
                 "ModelX", "1.0.0");
         // When
-        ResponseEntity<HubRegistrationResponse> response = restTemplate.postForEntity(url, request,
+        ResponseEntity<HubRegistrationResponse> response = restTemplate.postForEntity(REGISTER_URL, request,
                 HubRegistrationResponse.class);
         // Then
         assertTrue(response.getStatusCode().is2xxSuccessful());
@@ -57,32 +102,27 @@ public class RegisterHubControllerIntTest {
 
     @Test
     public void testConfirmHubSuccess() {
-        // // Given
+        // Given
+        User testUser = createUser("nickaldred@hotmail.co.uk", "Nick", "Aldred");
+        HubRegistrationResponse hubRegistrationResponse = registerHub(REGISTER_URL, REGISTER_URL);
 
-        // // First register a hub to get a valid OTP
-        // String registerUrl = "/api/v1/register/register";
-        // HubRegistrationRequest registerRequest = new HubRegistrationRequest(
-        // "ModelY", "2.0.0");
-        // ResponseEntity<HubRegistrationResponse> registerResponse =
-        // restTemplate.postForEntity(registerUrl,
-        // registerRequest, HubRegistrationResponse.class);
-        // assertTrue(registerResponse.getStatusCode().is2xxSuccessful());
-        // HubRegistrationResponse registrationBody = registerResponse.getBody();
-        // assertNotNull(registrationBody);
-
-        // // Add user to confirm hub
-
-        // // Now confirm the hub
-        // String confirmUrl = "/api/v1/register/confirm";
-        // RegisterHubController.HubConfirmRequest confirmRequest = new
-        // RegisterHubController.HubConfirmRequest(
-        // registrationBody.hubId(), null, registrationBody.otp());
-        // // When
-        // ResponseEntity<Void> confirmResponse = restTemplate.postForEntity(confirmUrl,
-        // confirmRequest,
-        // Void.class);
-        // // Then
-        // assertTrue(confirmResponse.getStatusCode().is2xxSuccessful());
+        // When
+        HubConfirmRequest confirmRequest = new HubConfirmRequest(
+                hubRegistrationResponse.hubId(), hubRegistrationResponse.otp(), testUser.getEmail());
+        ResponseEntity<Void> confirmResponse = restTemplate.postForEntity(CONFIRM_URL,
+                confirmRequest,
+                Void.class);
+        // Then
+        assertTrue(confirmResponse.getStatusCode().is2xxSuccessful());
+        Long hubId = hubRegistrationResponse.hubId();
+        assertNotNull(hubId);
+        Hub confirmedHub = hubRepository.findById(hubId).orElse(null);
+        assertNotNull(confirmedHub);
+        assertEquals(testUser.getEmail(), confirmedHub.getOwner().getEmail());
+        assertEquals(Hub.HubStatus.CONFIRMED, confirmedHub.getStatus());
+        assertEquals(null, confirmedHub.getOtp());
+        assertEquals(null, confirmedHub.getOtpExpiresAt());
+        assertEquals(null, confirmedHub.getOtpHash());
     }
 
     @Test
