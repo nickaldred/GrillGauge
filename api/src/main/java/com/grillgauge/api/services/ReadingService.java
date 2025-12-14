db164384096e501205e06d3b16021f0a17afce12
nicka <nickaldred@hotmail.co.uk>
2025-12-14 09:04:20 +0000

    tests working for revoked

--- api/src/main/java/com/grillgauge/api/services/ReadingService.java
+++ api/src/main/java/com/grillgauge/api/services/ReadingService.java
@@ -3,14 +3,12 @@ package com.grillgauge.api.services;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.grillgauge.api.domain.entitys.Probe;
 import com.grillgauge.api.domain.entitys.Reading;
 import com.grillgauge.api.domain.repositorys.ReadingRepository;
